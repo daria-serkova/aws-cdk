662f11809c187e72dc664b602f7d13acb6b2cc2b
dserkova <daria.serkova@pennep.com>
2024-07-22 17:02:50 -0400

    DMS: Minor refactoring (naming)

--- documents-management/lib/documents-management-stack.ts
+++ documents-management/lib/documents-management-stack.ts
@@ -2,12 +2,12 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cloudWatch from './cloud-watch';
 import * as s3 from './s3';
-import * as api from './api-gateway';
+import * as apiGateway from './api-gateway';
 export class DocumentsManagementStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const logs = cloudWatch.configureResources(this);
-    const bucket = s3.configureResources(this);
-    const apiGateway = api.configureResources(this);
+    const documentsStorage = s3.configureResources(this);
+    const api = apiGateway.configureResources(this);
   }
 }
