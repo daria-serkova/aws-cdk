b0dbc05490ebeb1090ee21b2f7127ebe317d8064
dserkova <daria.serkova@pennep.com>
2024-07-22 13:42:59 -0400

    DVS-AWS-TMP-GLUE-HC: Added synthetic data

--- aws-glue/healthcare-data-management/lib/s3.ts
+++ aws-glue/healthcare-data-management/lib/s3.ts
@@ -4,52 +4,58 @@ import { isProduction } from '../helpers/utilities';
 import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import path = require('path');
-const folders = [
+/**
+ * Defines S3 folders structure and location of test data files
+ * NOTE: S3 skips folder creation if no files are added to it. 
+ * This script uploads synthetic data or empty test.txt file if configured as `null` below.
+ */
+const s3Structure = [
     /* Contains raw data from various healthcare systems */
-    'raw/ehr/patient_data', 
-    'raw/ehr/clinical_trials',
-    'raw/ehr/lab_results',
-    'raw/billing/claims',
-    'raw/billing/payments',
-    'raw/external/',
+    { path: 'raw/ehr/patient_data', dataFolder: 'data/ehr/patient_data' }, // Data from https://synthetichealth.github.io/synthea/ 
+    { path: 'raw/ehr/clinical_trials', dataFolder: null },
+    { path: 'raw/ehr/lab_results', dataFolder: null },
+    { path: 'raw/billing/claims', dataFolder: null },
+    { path: 'raw/billing/payments', dataFolder: null },
+    { path: 'raw/external/', dataFolder: null },
 
     /* Contains data after initial processing or transformation. */
-    'processed/ehr/patient_data',
-    'processed/ehr/clinical_trials',
-    'processed/ehr/lab_results',
-    'processed/billing/claims',
-    'processed/billing/payments',
-    'processed/external',
+    { path: 'processed/ehr/patient_data', dataFolder: null },
+    { path: 'processed/ehr/clinical_trials', dataFolder: null },
+    { path: 'processed/ehr/lab_results', dataFolder: null },
+    { path: 'processed/billing/claims', dataFolder: null },
+    { path: 'processed/billing/payments', dataFolder: null },
+    { path: 'processed/external', dataFolder: null },
     
     /* Data ready for analytics purposes */
-    'curated/analytics/ehr/patient_data',
-    'curated/analytics/ehr/clinical_trials',
-    'curated/analytics/ehr/lab_results',
-    'curated/analytics/billing/claims',
-    'curated/analytics/billing/payments',
-    'curated/analytics/external',
+    { path: 'curated/analytics/ehr/patient_data', dataFolder: null },
+    { path: 'curated/analytics/ehr/clinical_trials', dataFolder: null },
+    { path: 'curated/analytics/ehr/lab_results', dataFolder: null },
+    { path: 'curated/analytics/billing/claims', dataFolder: null },
+    { path: 'curated/analytics/billing/payments', dataFolder: null },
+    { path: 'curated/analytics/external', dataFolder: null },
 
      /* Data formatted for reporting */
-    'curated/reporting/ehr/patient_data',
-    'curated/reporting/ehr/clinical_trials',
-    'curated/reporting/ehr/lab_results',
-    'curated/reporting/billing/claims',
-    'curated/reporting/billing/payments',
-    'curated/reporting/external',
+    { path: 'curated/reporting/ehr/patient_data', dataFolder: null },
+    { path: 'curated/reporting/ehr/clinical_trials', dataFolder: null },
+    { path: 'curated/reporting/ehr/lab_results', dataFolder: null },
+    { path: 'curated/reporting/billing/claims', dataFolder: null },
+    { path: 'curated/reporting/billing/payments', dataFolder: null },
+    { path: 'curated/reporting/external', dataFolder: null },
 
     /* Historical or archived data that is still important but not frequently accessed */
-    'curated/archives/ehr/patient_data',
-    'curated/archives/ehr/clinical_trials',
-    'curated/archives/ehr/lab_results',
-    'curated/archives/billing/claims',
-    'curated/archives/billing/payments',
-    'curated/archives/external',
+    { path: 'curated/archives/ehr/patient_data', dataFolder: null },
+    { path: 'curated/archives/ehr/clinical_trials', dataFolder: null },
+    { path: 'curated/archives/ehr/lab_results', dataFolder: null },
+    { path: 'curated/archives/billing/claims', dataFolder: null },
+    { path: 'curated/archives/billing/payments', dataFolder: null },
+    { path: 'curated/archives/external', dataFolder: null },
 
     /* Contains logs for monitoring and security */
-    'logs/glue_jobs',
-    'logs/access_logs',
-    'logs/security_audit'
+    { path: 'logs/glue_jobs', dataFolder: null },
+    { path: 'logs/access_logs', dataFolder: null },
+    { path: 'logs/security_audit',  dataFolder: null },
 ]
+
 
 /**
  * Function creates and configure S3 bucket.
@@ -67,13 +73,13 @@ export function configureResources(scope: Construct, bucketName: string) {
         autoDeleteObjects: true,
         blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     });
-    // Creates folders structure
-    const sourceDir = path.join(__dirname, '../data');
-    folders.forEach(folder => {
-        new BucketDeployment(scope, `Deploy-${folder}`, {
-            sources: [Source.asset(sourceDir)], // Source directory containing the file to copy
+    // Creates folders structure and uploads test files into it.
+    s3Structure.forEach(folder => {
+        const sourceDir = folder.dataFolder ? path.join(__dirname, `../${folder.dataFolder}`) : path.join(__dirname, `../data/test`);
+        new BucketDeployment(scope, `Deploy-${folder.path}`, {
+            sources: [ Source.asset(sourceDir) ], // Source directory containing the file to copy
             destinationBucket: s3Bucket,
-            destinationKeyPrefix: folder,
+            destinationKeyPrefix: folder.path,
             //prune: false
         });
     });
