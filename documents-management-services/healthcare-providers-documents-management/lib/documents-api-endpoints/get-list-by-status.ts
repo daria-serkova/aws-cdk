1ad65903eaefc52165ae92f3d08ceabe205a2eea
daria-serkova <daria.serkova@gmail.com>
2024-07-23 11:53:49 -0400

    DMS: Added Get List by Status api endpoint

---
+++ documents-management-services/healthcare-providers-documents-management/lib/documents-api-endpoints/get-list-by-status.ts
@@ -0,0 +1,75 @@
+import { Construct } from 'constructs';
+import { Duration } from 'aws-cdk-lib';
+import { Policy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { JsonSchemaType, LambdaIntegration, Resource } from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { resolve, dirname } from 'path';
+import { ApiResources, LogsResources } from '../../helpers/types';
+import { 
+    AWS_REQUETS_MODELS_NAMING_CONVENTION, 
+    AWS_RESOURCES_NAMING_CONVENTION, 
+    REGION, 
+    VALIDATION_RULES 
+} from '../../helpers/utilities';
+
+const resourcesNames = {
+    lambda:  AWS_RESOURCES_NAMING_CONVENTION.replace('$', 'get-list-by-status'),
+    iamRole: AWS_RESOURCES_NAMING_CONVENTION.replace('$', 'get-list-by-status-role'),
+    iamPolicy: AWS_RESOURCES_NAMING_CONVENTION.replace('$', 'get-list-by-status-policy'),
+    apiEndpoint: 'get-list-by-status',
+    apiRequestModel: `${AWS_REQUETS_MODELS_NAMING_CONVENTION}GetListByStatus`
+};
+export default function getListByStatusAction(
+    scope: Construct, 
+    lambdasFolder: string, 
+    logs: LogsResources,
+    api: ApiResources,
+    apiNode: Resource
+) {
+    const iamRole = new Role(scope, resourcesNames.iamRole, {
+        assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+        roleName: resourcesNames.iamRole,
+    });
+    iamRole.attachInlinePolicy(
+        new Policy(scope, resourcesNames.iamPolicy, {
+            policyName: resourcesNames.iamPolicy,
+            statements: [logs.policy],
+        })
+    );
+    const lambda = new NodejsFunction(scope, resourcesNames.lambda, {
+        functionName: resourcesNames.lambda,
+        description: 'Lambda function to handle documents retreival by document status',
+        entry: resolve(dirname(__filename), `${lambdasFolder}/${resourcesNames.apiEndpoint}.ts`),
+        memorySize: 256,
+        timeout: Duration.minutes(3),
+        handler: 'handler',
+        logGroup: logs.group,
+        role: iamRole,
+        environment: {
+            REGION: REGION,
+        },
+    });
+    const requestModel = {
+        contentType: "application/json",
+        description: `Validates parameters for Get List of documents by status API endpoint`,
+        modelName: resourcesNames.apiRequestModel,
+        modelId: resourcesNames.apiRequestModel,
+        schema: {
+            type: JsonSchemaType.OBJECT,
+            properties: {
+                ip: {
+                    type: JsonSchemaType.STRING,
+                    pattern: VALIDATION_RULES.PATTERN_IP,
+                },
+            },
+            required: ["ip"],
+        },
+    };
+    api.apiGateway.addModel(requestModel.modelName, requestModel);
+    const action = apiNode.addResource(resourcesNames.apiEndpoint);
+    action.addMethod("POST", new LambdaIntegration(lambda), {
+        apiKeyRequired: true,
+        requestModels: { "application/json": requestModel },
+        requestValidator: api.requestValidator,
+    });
+}
--- documents-management-services/healthcare-providers-documents-management/lib/documents-api-endpoints/index.ts
+++ documents-management-services/healthcare-providers-documents-management/lib/documents-api-endpoints/index.ts
@@ -5,6 +5,7 @@ import getDocumentMetadataAction from './get-metadata';
 import getListByUserAction from './get-list-by-user';
 import updateDocumentMetadataAction from './update-metadata';
 import { ApiResources, LogsResources } from '../../helpers/types';
+import getListByStatusAction from './get-list-by-status';
 
 const lambdasFolder = '../../functions/documents-api';
 const apiNodeName = 'documents';
@@ -22,5 +23,6 @@ export default function configureDocumentsApiResources(
         getDocumentMetadataAction(scope, lambdasFolder, logs, api, apiNode);
         getListByUserAction(scope, lambdasFolder, logs, api, apiNode);
         updateDocumentMetadataAction(scope, lambdasFolder, logs, api, apiNode);
+        getListByStatusAction(scope, lambdasFolder, logs, api, apiNode);
     }
 }
