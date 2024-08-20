d571d7961bff8b9f5fa759619064e05bc94e406e
daria-serkova <daria.serkova@gmail.com>
2024-08-20 09:34:06 -0400

    DAS: Removed obsolete code

--- analytics-services/healthcare-data-management/bin/healthcare-data-management.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { HealthcareDataManagementStack } from '../lib/healthcare-data-management-stack';
-import environmentConfig from './stack-configs';
-
-const app = new cdk.App();
-new HealthcareDataManagementStack(app, 'HealthcareDataManagementStack', environmentConfig);
